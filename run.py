3c83aecb2f319a847fbfa0db55c8021f9ddc9650
Tim Patullock <oddalicious@gmail.com>
2024-07-05 23:19:49 +1000

    remove specific .env calling and use the load_dotenv instead

--- run.py
+++ run.py
@@ -2,18 +2,17 @@
 import streamlit as st
 import chromadb
 from langchain.chains.question_answering import load_qa_chain
-from dotenv import dotenv_values
-import os
+from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
 from update_collection import update_collection
 
-os.environ['OPENAI_API_KEY'] = dotenv_values('.env')['OPENAI_API_KEY'] # type: ignore
+load_dotenv()
 
 def create_agent_chain():
-    model_name = "gpt-3.5-turbo"
-    llm = ChatOpenAI(model_name=model_name) # type: ignore
+    model = "gpt-3.5-turbo"
+    llm = ChatOpenAI(model=model)
     chain = load_qa_chain(llm, chain_type="stuff")
     return chain
 
@@ -23,7 +22,6 @@ def get_llm_response(query):
     client = chromadb.HttpClient(host='localhost', port=8000)
     vectordb = Chroma(client=client, collection_name='sourcebooks', embedding_function=OpenAIEmbeddings())
 
-    #vectordb = Chroma(persist_directory="./chroma-store", embedding_function=OpenAIEmbeddings())
     matching_docs = vectordb.similarity_search(query)
     answer = chain.invoke({"input_documents": matching_docs, "question": query})
     return answer['output_text']
